6ad9af7720b35c0fd6a8114403669eee9fdcc8b3
Ali Nix <anix1@kent.edu>
2022-02-07 20:26:01 -0700

    final copy variables.js

--- variables.js
+++ variables.js
@@ -1,11 +1,14 @@
 var dogName = 'Mika';
 var catName = 'Melody';
 var mouseName = 'Ted';
+// animal variables with names //
 
 var dogWeight = 51;
 var catWeight = 25;
 var mouseWeight = 0.25;
+// animal weights //
 
 var dogHappy = true;
 var catSad = false;
 var mouseHappy = false;
+// animal contentment //
